b586a95470f2d64d5cdcf8e1a88c536341757ac1
Bruce Denham <bdenham@adobe.com>
2024-07-25 10:02:11 -0500

    change delivery block

--- blocks/delivery-confirmation/delivery-confirmation.js
+++ blocks/delivery-confirmation/delivery-confirmation.js
@@ -3,9 +3,11 @@ export default function decorate(block) {
   const deliveryConfirmation = document.createElement('div');
   deliveryConfirmation.classList.add('delivery-confirmation');
   deliveryConfirmation.innerHTML = `
-    <div class="delivery-recipient">John Smith</div>
-    <div class="delivery-address">555 Seaside Dr, Seaside, WA, 77777-5555, United States</div>
-    <a href="#" title="Change" class="button">Change</a>
+    <div class="delivery-recipient">
+    <div>John Smith</div>
+    <span class="delivery-address">555 Seaside Dr, Seaside, WA, 77777-5555, United States</span>
+    </div>
+    <a href="#" title="Change" class="delivery-confirmation-button">Change</a>
   `;
   block.textContent = '';
   block.append(deliveryConfirmation);
