dce2ed2b62de6322ef9e3607bb0c1ba6c848e111
Bruce Denham <bdenham@adobe.com>
2024-07-25 09:30:03 -0500

    change delivery block

--- blocks/delivery-confirmation/delivery-confirmation.js
+++ blocks/delivery-confirmation/delivery-confirmation.js
@@ -1,15 +1,12 @@
 export default function decorate(block) {
   /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'delivery-confirmation-image';
-      else div.className = 'delivery-confirmation-body checkout__main';
-    });
-    ul.append(li);
-  });
+  const deliveryConfirmation = document.createElement('div');
+  deliveryConfirmation.classList.add('delivery-confirmation');
+  deliveryConfirmation.innerHTML = `
+    <div class="delivery-recipient">John Smith</div>
+    <div class="delivery-address">555 Seaside Dr, Seaside, WA, 77777-5555, United States</div>
+    <a href="#" title="Change" class="button">Change</a>
+  `;
   block.textContent = '';
-  block.append(ul);
+  block.append(deliveryConfirmation);
 }
